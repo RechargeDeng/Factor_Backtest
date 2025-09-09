63fe9cf5bb21e4f8653ada86e16d226bb17c5cda
D <96329340+RechargeDeng@users.noreply.github.com>
2025-09-10 00:01:19 +0800

    debug

--- AutoFormula/operations_cy/one.pyx
+++ AutoFormula/operations_cy/one.pyx
@@ -1,11 +1,23 @@
-# Copyright (c) 2021 Dai HBG
+# Copyright 2025 Jason Deng
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 
 
 """
 该代码是1型运算符的cython版本源代码，注意需要为不同维度都写操作
 
 日志
-2022-01-20
 - 新增log操作和logv操作
 """
 
